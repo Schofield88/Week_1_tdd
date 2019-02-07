eeeba1f415714486ffdc1fa3f145e3c14c694563
Luke Phyall <lukeprospect@yahoo.com>
2019-02-07 10:27:55 +0000

    Adds argument to second test. Makes everything work.

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -5,7 +5,7 @@ describe Dice do
   it { is_expected.to respond_to :roll }
 
   it 'roll between 1 and 6' do
-    expect(subject.roll).to be_between(1, 6).inclusive
+    expect(subject.roll(1)).to be_between(1, 6).inclusive
   end
 
   it { is_expected.to respond_to(:roll).with(1).argument }
