51005ac28059c80245419227737b436711434201
Luke Phyall <lukeprospect@yahoo.com>
2019-02-07 10:13:28 +0000

    Adds correct syntax for be_between in second test. Code passes.

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -5,7 +5,7 @@ describe Dice do
   it { is_expected.to respond_to :roll }
 
   it 'roll between 1 and 6' do
-    expect(subject.roll).to eq (1..6)
+    expect(subject.roll).to be_between(1, 6).inclusive
   end
 
 end
